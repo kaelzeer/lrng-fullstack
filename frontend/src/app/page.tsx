7e4e3380b7072969cbcbdffad4d991a0234383c5
nrosto <nrotolamak@gmail.com>
2024-02-26 06:10:47 +0900

    add footer
    https://app.asana.com/0/0/1206663848661214/f
    - added footer.tsx
    - added styles for footer
    - added footer component to page.tsx
    - added visible for footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from './header';
+import Footer from './footer';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <div> Hello World ! </div>
+      <Footer/>
     </div>
   );
 }
