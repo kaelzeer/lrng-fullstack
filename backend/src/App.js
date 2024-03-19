5b7d4204ad0c86af019a8b358611a3fabe6429bb
Dmitrii Mukhin <uufswe@gmail.com>
2024-03-19 18:06:01 +0300

    add swagger docs
    https://github.com/users/kaizxr/projects/1?pane=issue&itemId=55512663
    -add swagger documentaion for `/posts`

--- backend/src/App.js
+++ backend/src/App.js
@@ -3,6 +3,9 @@ const { serverPort, mongoDBUri } = require('./utils/Constants')
 var express = require('express')
 var app = express()
 
+const swaggerHandler = require('./utils/swagger/SwaggerHandler')
+swaggerHandler.handle(app)
+
 const MongoDBHandler = require('./utils/mongodb/MongoDBHandler')
 const mongoDBHandler = new MongoDBHandler(mongoDBUri)
 
