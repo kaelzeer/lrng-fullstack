c85906097f3d803a0d3fe9b99e8d4b6ed688a3e6
Dmitrii Mukhin <uufswe@gmail.com>
2024-03-19 18:28:16 +0300

    indentation fix pt. II
    https://github.com/users/kaizxr/projects/1/views/1?pane=issue&itemId=55512663
    - fix SwaggerHandler.js

--- backend/src/utils/swagger/SwaggerHandler.js
+++ backend/src/utils/swagger/SwaggerHandler.js
@@ -2,7 +2,7 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json')
 
 function handleSwagger(app) {
-	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 }
 
 module.exports = { handle: handleSwagger }
