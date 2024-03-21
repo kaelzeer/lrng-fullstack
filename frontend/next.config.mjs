3f25e7ba1354b1242ef4a153d4368ca5cc6d89e6
gosha <ewws@mail.ru>
2024-03-21 14:02:15 +0300

    testing api

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, 
-    images : { 
-        domains : ['fineproxy.org'] 
-  } 
+  reactStrictMode: true,
+  images: {
+    domains: ["fineproxy.org"],
+  },
+
 };
 
 export default nextConfig;
--- frontend/src/components/mainPageComponent.tsx
+++ frontend/src/components/mainPageComponent.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import Link from 'next/link';
-import PostBlock from '@/components/postBlock';
-import postsData from '@/data/postsData';
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import PostBlock from "@/components/postBlock";
+import postsData from "@/data/postsData";
+import axios from "axios";
 
 const MainPageComponent: React.FC = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("/api");
+      setData(data);
+    };
+    getData();
+  }, []);
   return (
     <div className={"mainPageComponent"}>
       <div className={"mainLeft"}>Hello, World1</div>
       <div className={"mainMid"}>
-        {postsData.map(post => 
-        (
+        {data.map((post) => (
           <Link href={`/post/${post.id}`} key={post.id}>
             <PostBlock post={post} />
           </Link>
-        )) 
-        }
+        ))}
       </div>
       <div className={"mainRight"}>Hello, World3</div>
     </div>
