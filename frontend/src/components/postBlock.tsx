0c15860fc6d8eca6362bc126e3e10baa62fd8dc0
nrosto <nrotolamak@gmail.com>
2024-03-24 23:01:13 +0900

    rework styles
    https://github.com/users/kaizxr/projects/1/views/1?pane=issue&itemId=57203737
    - deleted Profile in header.tsx
    - added black circle
    - changed mainLeft and mainRight styles
    - added visual icons for likes, views and share

--- frontend/src/components/postBlock.tsx
+++ frontend/src/components/postBlock.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HeartIcon, EyeIcon, ShareIcon } from '@heroicons/react/16/solid';
 
 const PostBlock: React.FC<{ post: any }> = ({ post }) => {
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
   return (
     <div className={"postBlock"}>
       <h2 className={"blockHeader"}> Header: {post.header}</h2>
       <p className={"content"}>Text: {post.text}</p>
       <div className={"statistics"}>
-        {/* Временно уберу */}
-        {/* <span className={"likes"}>Likes: {post.likes}</span>
-        <span className={"views"}>Views: {post.views}</span>
-        <button className={"actionButton"}>Share</button> */}
+        <a className="icon" onClick={(e) => { e.preventDefault(); toggleLike(); }}>
+          <HeartIcon className={`icon ${isLiked ? 'liked' : ''}`} />
+        </a>
+        <EyeIcon className="icon" />
+        <ShareIcon className="icon" />
       </div>
     </div>
   );
