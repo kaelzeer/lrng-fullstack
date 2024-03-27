1daec252b590affeb69befdc73f167adcea62910
Dmitrii Mukhin <uufswe@gmail.com>
2024-03-27 09:46:54 +0300

    move /posts endpoint to /api/posts
    https://github.com/users/kaizxr/projects/1/views/1?pane=issue&itemId=57202490
    - add new GET endpoint /api/posts;
    - mark old GET endpoint /posts as to be deprecated soon;

--- backend/src/router/AppRouter.js
+++ backend/src/router/AppRouter.js
@@ -3,7 +3,18 @@ function addRoutes(app, mongoDBHandler) {
     res.send('Hello World!')
   })
 
+  // will be deprecated in [1.1.0]
   app.get('/posts', async (req, res) => {
+    try {
+      const data = await mongoDBHandler.getAllPosts()
+      res.json(data)
+    } catch (error) {
+      console.error("Error fetching posts:", error)
+      res.status(500).send("Internal Server Error")
+    }
+  })
+
+  app.get('/api/posts', async (req, res) => {
     try {
       const data = await mongoDBHandler.getAllPosts()
       res.json(data)
