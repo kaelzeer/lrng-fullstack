f3326e0fd9c6cb4e96570e694a587f34d8a90a4a
nrosto <nrotolamak@gmail.com>
2024-04-04 18:43:59 +0900

    Dockers
    https://github.com/users/kaelzeer/projects/1?pane=issue&itemId=54734324
    - added dockerfile for backend + frontend
    - added docker-compose.yml

--- backend/src/utils/Constants.js
+++ backend/src/utils/Constants.js
@@ -1,6 +1,6 @@
 const serverPort = 7293;
 const serverHostname = "0.0.0.0";
-const mongoDBUri = "mongodb://127.0.0.1:27017";
+const mongoDBUri = "mongodb://mongo:27017";
 
 module.exports = {
   serverPort,
